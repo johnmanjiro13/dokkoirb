bfd56f26bd41947603eab7c7c646eb6549792f54
johnmanjiro13 <28798279+johnmanjiro13@users.noreply.github.com>
2022-12-09 13:09:23 +0900

    feat: Add help and select commands

--- bot.rb
+++ bot.rb
@@ -3,14 +3,21 @@
 require 'discordrb'
 require 'sinatra'
 
+# http server for health checking
 get '/ping' do
   'pong'
 end
 
+# discord bot
 bot = Discordrb::Bot.new token: ENV['DISCORD_TOKEN']
 
-bot.message(with_text: 'Ping!') do |event|
-  event.respond 'Pong!'
+bot.message(with_text: /^dokkoi\s+help/) do |event|
+  content = '```
+dokkoi help - Displays all of the help commands that this bot knows about.
+dokkoi echo <text> - Reply back with <text>.
+dokkoi select <element1>,<element2>,... - Choose one of the elements in your list randomly.
+```'
+  event.respond content
 end
 
 ECHO_REGEXP = /^dokkoi\s+echo\s+(.+)/
@@ -19,4 +26,11 @@ bot.message(with_text: ECHO_REGEXP) do |event|
   event.respond content
 end
 
-bot.run(:background)
+SELECT_REGEXP = /^dokkoi\s+select\s+(.+)/
+bot.message(with_text: SELECT_REGEXP) do |event|
+  options_str = SELECT_REGEXP.match(event.content)[1]
+  options = options_str.gsub(/[.!?]/, '').split(/[,\s]/)
+  event.respond options.sample
+end
+
+bot.run(background=true)
